869c1b4e779bb9ad0c193d12fdc2da02c864797d
Renee de Kruijf <renee@webdesignpuntnl.com>
2018-03-30 20:32:01 +0200

    second commit finished setting up

--- _source/js/webdesignpuntnl-script.js
+++ _source/js/webdesignpuntnl-script.js
@@ -1,5 +1,4 @@
 var heading = document.getElementById('h1');
 heading.addEventListener('click', function() {
     console.log('werkt');
-    
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var prefix = require('gulp-autoprefixer');
-var imagemin = require('gulp-imagemin');
-var paths = {
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const prefix = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const purifycss = require('gulp-purifycss');
+const paths = {
     scss: './_source/scss/*.scss'
 };
 
@@ -24,22 +25,53 @@ gulp.task('scripts', function(){
 });
 
 
-// sass
+// sass compiling
 gulp.task('sass', function(){
     gulp.src('_source/scss/webdesignpuntnl-styles.scss')
+    .pipe(sourcemaps.init())
         .pipe(sass({
+            outputStyle: 'expanded', // nested, compact, expanded, compressed
             includePaths: ['scss']
         }))
+        .on('error', console.error.bind(console))
+        .pipe(prefix())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('www/css'));
 });
+
+// remove unused css
+gulp.task('css', function() {
+    return gulp.src('./www/css/webdesignpuntnl-styles.css')
+      .pipe(purifycss(['./www/**/*.js', './www/**/*.html']))
+      .pipe(gulp.dest('./www/css'));
+});
+
+// browser-sync
 gulp.task('browser-sync', function(){
-    browserSync.init(["www/css/*.css", "www/js/*.js", "www/*.html", "www/pages/*.html"], {
+    browserSync.init(["www/css/*.css", "www/js/*.js", "www/*.html", "www/pages/*.html" , "www/media/images/*"], {
         server: {
             baseDir: "www"
         }
     })
 });
 
-gulp.task('default', ['sass', 'browser-sync', 'scripts'], function(){
-    gulp.watch(["_source/scss/*.scss", "www/*.html", "www/pages/*.html"], ['sass'], ['scripts']);
+// optimize images 
+    gulp.task('image', function(){
+    gulp.src('_source/images/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+	        imagemin.jpegtran({progressive: true}),
+	        imagemin.optipng({optimizationLevel: 5}),
+	        imagemin.svgo({
+		        plugins: [
+			        {removeViewBox: true},
+			        {cleanupIDs: false}
+		        ]
+	        })
+        ]))
+        .pipe(gulp.dest('www/media/images'));
+    });
+
+gulp.task('default', ['sass', 'browser-sync', 'scripts', 'image', 'css'], function(){
+    gulp.watch(["_source/scss/*.scss", "_source/js/*.js", "_source/images/*", "www/*.html", "www/pages/*.html", "www/js/*.js"], ['sass', 'scripts', 'image', 'css']);
 });
