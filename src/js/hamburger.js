89535db0614858acfc07af65c88a25d1c7331626
Renee de Kruijf <renee@webdesignpuntnl.com>
2018-10-31 09:43:34 +0100

    finished the css for the header

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,7 +1,9 @@
 const hamburgerMenu = document.querySelector('.menuSmall');
+const topMenu = document.querySelector('.header__nav__menu');
 
 function handleMenu() {
   hamburgerMenu.classList.toggle('change');
+  topMenu.classList.toggle('hideMenu');
 }
 
 hamburgerMenu.addEventListener('click', handleMenu);
