8ec0b0449aa9513f25d4bc8a82f07c51c4ebe0ec
Renee de Kruijf <renee@webdesignpuntnl.com>
2018-12-05 11:40:23 +0100

    menu and hamburger icon stay visible on page change

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,30 +1,32 @@
-//! het menu moet alleen zichtaar zijn als er geklikt
-//! is niet bij de eerste paginaload en niet als het weg geklikt is
-
 const nav = document.querySelector('.header__nav');
 let getHamburgerMenu = document.querySelector('.menuSmall');
 const topMenu = document.querySelector('.header__nav__menu');
 const hamburgerMenu = document.createElement('div');
 
+// hide the menu on pageload: shows the menu if no js
 getHamburgerMenu.parentNode.removeChild(getHamburgerMenu);
 
+// show/hide menu and icon on resize
 function windowResize() {
-  const windowSize = window.innerWidth;
+  const windowSize = window.innerWidth; // get windowsize
   getHamburgerMenu = document.querySelector('.menuSmall');
   if (windowSize >= 600) {
     getHamburgerMenu.style.display = 'none';
-    console.log(typeof (getHamburgerMenu));
   } else if (windowSize <= 600) {
     getHamburgerMenu.style.display = 'block';
   }
 }
 
+// open or close the menu.Menu stays open on pagechange
 function handleMenu() {
   const newHamburgerMenu = document.querySelector('.menuSmall');
-  newHamburgerMenu.classList.toggle('change');
-  topMenu.classList.toggle('hideMenu');
+  if (topMenu.dataset.show === '2') {
+    newHamburgerMenu.classList.toggle('change');
+    topMenu.classList.toggle('hideMenu');
+  }
 }
 
+// markup for the hamburgermenu
 const markup = `
   <div class="menuSmall change" id="hamburgerIcon" role="button">
     <div class="menuSmall__line01">Lijn01</div>
@@ -32,8 +34,19 @@ const markup = `
     <div class="menuSmall__line03">Lijn03</div>
   </div>`;
 
+// put the html into the dom
 hamburgerMenu.innerHTML = markup;
 nav.appendChild(hamburgerMenu);
 
+// call the resize function
+windowResize();
+
+// hide the menu on pageload
+if (topMenu.dataset.show === '1') {
+  handleMenu();
+  topMenu.dataset.show = '2';
+}
+
+// event listeners
 hamburgerMenu.addEventListener('click', handleMenu);
 window.addEventListener('resize', windowResize);
