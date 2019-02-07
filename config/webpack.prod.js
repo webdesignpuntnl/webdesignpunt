0cec418c2888568780745d75dcb9de379e0cee74
Renee de Kruijf <renee@webdesignpuntnl.com>
2019-02-07 18:42:41 +0100

    fixed a bug on the error pages not loading the css properly

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,12 +3,12 @@
 const path = require('path');
 const glob = require('glob');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCss = require('optimize-css-assets-webpack-plugin');
 const PurgecssPlugin = require('purgecss-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const {ImageminWebpackPlugin} = require('imagemin-webpack');
+const ImageminWebpackPlugin = require('imagemin-webpack');
 const imageminGifsicle = require("imagemin-gifsicle"); // losles
 const imageminJpegtran = require("imagemin-jpegtran"); // losless
 const imageminMozjpeg = require("imagemin-mozjpeg"); // lossy
@@ -18,18 +18,18 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const plugins = [imageminGifsicle(), imageminMozjpeg(), imageminOptinpng(), imageminSvgo()];
 
-const utils = require('./utils');
+const utils = require('./utils-prod');
 const utilsErrorDocs = require('./utilsErrorDocs');
 
 const PATHS = { src: path.join(__dirname, '../src') }
 
-module.exports =  {
-    
+module.exports = {
+
     entry: {
-        main: [ "./main.js" ],
+        main: ["./main.js"],
         contactForm: ["./src/js/formvalidate.js"]
     },
-    mode: "production",   
+    mode: "production",
     output: {
         filename: "js/[name]-bundle.js",
         path: path.resolve(__dirname, "../dist"),
@@ -39,22 +39,22 @@ module.exports =  {
         rules: [
             {
                 test: /\.s?[ac]ss$/,
-                use: [ 
-                    
+                use: [
+
                     { loader: MiniCssExtractPlugin.loader, options: { publicPath: "../" } },
                     { loader: "css-loader", options: { url: true, sourceMap: true } },
                     { loader: "postcss-loader", options: { sourceMap: true } },
-                    { loader: "sass-loader", options: { sourceMap: true} }
+                    { loader: "sass-loader", options: { sourceMap: true } }
                 ]
             },
             {
-                test: /\.js$/, 
-                exclude: /node_modules/, 
+                test: /\.js$/,
+                exclude: /node_modules/,
                 loader: "babel-loader"
             },
             {
                 test: /\.pug$/,
-                use: [ 
+                use: [
                     { loader: "pug-loader" }
                 ]
             },
@@ -68,7 +68,7 @@ module.exports =  {
                             // publicPath: function(url) {
                             //     return url.replace(/images/, '../images')
                             // }
-                        } 
+                        }
                     }
                 ]
             }
@@ -77,16 +77,16 @@ module.exports =  {
     optimization: {
         minimizer: [new UglifyJsPlugin()]
     },
-    devtool: "inline-source-map", 
+    devtool: "inline-source-map",
     plugins: [
         new CleanWebpackPlugin(['dist'], {
             root: path.join(__dirname, '..')
-          }),
+        }),
         new MiniCssExtractPlugin({
             filename: "css/[name].css",
         }),
         new PurgecssPlugin({
-            paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+            paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
         }),
         new OptimizeCss({
 
@@ -115,17 +115,19 @@ module.exports =  {
                 nameCache: null,
                 ie8: false,
                 keep_fnames: false,
-              }
+            }
         }),
         new CopyWebpackPlugin([
-           { from: 'src/assets/**/*',
-             to: '[name].[ext]'
+            {
+                from: 'src/assets/**/*',
+                to: '[name].[ext]'
             }
         ]),
         new CopyWebpackPlugin([
-            { from: 'src/php/**/*',
-              to: 'php/[name].[ext]'
-             }
-         ]) 
+            {
+                from: 'src/php/**/*',
+                to: 'php/[name].[ext]'
+            }
+        ])
     ]
 };
