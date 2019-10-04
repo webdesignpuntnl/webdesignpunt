e9b459443ad3987ab4b45e944b4cf743ce5531d4
Renee de Kruijf <renee@webdesignpuntnl.com>
2019-10-04 14:27:33 +0200

    added in production
    added a in production section to the projects page

--- src/js/poppupInfo.js
+++ src/js/poppupInfo.js
@@ -11,6 +11,19 @@ const poppupInfo = [
     content:
       'Het buurtpanel is een onafhankelijke groep bewoners uit alle delen van Culemborg, die geld beschikbaar stelt voor initiatieven die de leefbaarheid van uw wijk verhogen.',
     link: 'https://www.buurtpanelculemborg.nl'
+  },
+  {
+    client: 'Sylke van de Calseijde Theatermaker',
+    image: '../images/screenshot-theatermaker.png',
+    content: 'Sylke van de Calsijede is een freelance theatermaker. Ze is vooral gespecialiseerd in kindertheater.',
+    link: '#'
+  },
+  {
+    client: 'Stichting Echelon',
+    image: '../images/screenshot-echelon.png',
+    content:
+      'Echelon is er voor iedereen die iets wil doen in de vormgeving van onderwijs over levensbeschouwing, communicatie en identiteitsontwikkeling.',
+    link: '#'
   }
 ];
 
