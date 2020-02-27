2a725d5b7f3532660b90b0911f9d65a26afa9ba7
Renee de Kruijf <renee@webdesignpuntnl.com>
2020-02-27 21:44:00 +0100

    added skip-link blog

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
   "presets": ["@babel/preset-env"],
   "plugins": [
     ["prismjs", {
-      "languages": ["javascript"],
+      "languages": ["javascript", "html", "css"],
       "plugins": ["normalize-whitespace"],
       "theme": "okaidia",
       "css": true
